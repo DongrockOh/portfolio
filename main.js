0a193fab4458b1549f1eab73badeeca86544011c
DongrockOh <87560481+DongrockOh@users.noreply.github.com>
2021-12-10 12:04:10 +0900

    JS: Home Contact Button

--- main.js
+++ main.js
@@ -20,6 +20,16 @@ navbarMenu.addEventListener("click", (event) => {
   if (link == null) {
     return;
   }
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView(link);
 });
+
+// Handle click on "contact me" button on home
+const homeCantactBtn = document.querySelector(".home__contact");
+homeCantactBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
